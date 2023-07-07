28100627c2cc3ef0568e146c9503fb046260ba01
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-07 20:41:39 +0200

    Add basic RSS parsing

--- sylvia.py
+++ sylvia.py
@@ -1,8 +1,12 @@
 import argparse
+import sylvia.render
+import feedparser
 
 parser = argparse.ArgumentParser(description='sylvia - generate RSS-based event calendars')
 parser.add_argument('rss_path', type=str,
 					help='Path to the RSS feed the calendar should be based on')
 args = parser.parse_args()
 
-print(args.rss_path)
+rss = feedparser.parse(args.rss_path)
+
+print(sylvia.render.calendar(rss))
