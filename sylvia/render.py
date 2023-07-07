14ead87053534aeb8427af9e248eb8c7e79206c5
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-07 20:41:18 +0200

    Implement basic rendering

--- sylvia/render.py
+++ sylvia/render.py
@@ -1,11 +1,11 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
-    loader=PackageLoader("html"),
+    loader=PackageLoader("website"),
     autoescape=select_autoescape()
 )
 
 template = env.get_template("calendar.html")
 
-def calendar(events):
-    return template.render()
+def calendar(rss):
+    return template.render(rss=rss)
