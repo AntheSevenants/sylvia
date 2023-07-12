dd995cc98805029a3b1db35470072beea98d76fc
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-12 16:08:46 +0200

    Add date parsing to calendar

--- sylvia/render.py
+++ sylvia/render.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(
@@ -7,5 +9,22 @@ env = Environment(
 
 template = env.get_template("calendar.html")
 
+def date_from_string(input_datetime):
+    input_datetime = input_datetime.split("+")[0]
+    return datetime.strptime(input_datetime, f"%Y-%m-%dT%H:%M:%S")
+
+def print_date(input_datetime):
+    event_timestamp = date_from_string(input_datetime)
+
+    return event_timestamp.strftime("%-d %B %Y")
+
+def print_time(input_datetime):
+    event_timestamp = date_from_string(input_datetime)
+
+    return event_timestamp.strftime("%-H:%M")
+
+
 def calendar(rss):
-    return template.render(rss=rss)
+    return template.render(rss=rss,
+                           print_date=print_date,
+                           print_time=print_time)
