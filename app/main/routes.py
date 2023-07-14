4314341152bf76163292e89dcffd3171529313e3
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-14 14:50:48 +0200

    Add calendar generation logic to interface

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,11 @@
-import sylvia.diff
+import os
+import json
 
-from flask import session, redirect, url_for, render_template, request, send_file, current_app
+import sylvia.error
+import sylvia.diff
+import sylvia.render
+
+from flask import session, redirect, url_for, render_template, request, send_file, current_app, Response
 from . import main
 
 print("routes are loaded")
@@ -11,3 +16,36 @@ def index():
     cache_files = sylvia.diff.get_cache_files("cache/")
 
     return render_template('index.html', cache_files=cache_files)
+
+@main.route('/calendar', methods = ["GET", "POST"])
+def calendar():
+    if request.method != "POST":
+        return sylvia.error.generate("This endpoint only accepts POST requests.")
+    
+    if not "changes_source" in request.form:
+        return sylvia.error.generate("Malformed POST request.")
+    
+    cache_file = request.form["changes_source"]
+    cache_old = sylvia.diff.get_cache_from_path(cache_file)
+
+    calendar_html = sylvia.render.calendar(cache_old)
+
+    return render_template('calendar.html', calendar_html=calendar_html, cache_file=cache_file)
+
+
+@main.route('/download', methods = ["GET", "POST"])
+def download():
+    if request.method != "POST":
+        return sylvia.error.generate("This endpoint only accepts POST requests.")
+    
+    if not "changes_source" in request.form:
+        return sylvia.error.generate("Malformed POST request.")
+    
+    cache_file = request.form["changes_source"]
+    cache_old = sylvia.diff.get_cache_from_path(cache_file)
+
+    calendar_html = sylvia.render.calendar(cache_old)
+
+    return Response(calendar_html,
+                    mimetype="text/html",
+                    headers={"Content-disposition": "attachment; filename=calendar.html"})
