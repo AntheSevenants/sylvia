818afd615ad7c94ad1d06adea449d642ec0cae27
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-14 14:51:58 +0200

    Check for environment variables

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
+import os
 import argparse
 from app import create_app
 
 parser = argparse.ArgumentParser(description='Run sylvia frontend')
 
 args = parser.parse_args()
+
+environment_variables = [ "RSS_URL", "CACHE_DIR" ]
+
+for environment_variable in environment_variables:
+	if not environment_variable in os.environ:
+		raise Exception(f"Environment variable '{environment_variable}' missing")
 
 app = create_app(debug=True)
 app.jinja_env.auto_reload = True
