7337d676f599fa02a9c12ffe14351909c9e944ea
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-14 14:51:48 +0200

    Add render pipeline

--- sylvia/render.py
+++ sylvia/render.py
@@ -1,3 +1,7 @@
+import os
+import feedparser
+import sylvia.diff
+
 from datetime import datetime
 
 from jinja2 import Environment, PackageLoader, select_autoescape
@@ -28,7 +32,26 @@ def print_date_time(input_datetime):
 
     return event_timestamp.strftime("%-d %B %Y %H:%M")
 
-def calendar(rss):
+def calendar(cache_old=None):
+    # Retrieve the RSS feed
+    rss = feedparser.parse(os.environ["RSS_URL"])
+    rss = rss["entries"]
+    # Turn it into "new" cache
+    cache_new = sylvia.diff.get_cache(rss)
+
+    # Only diff and enrich if cache is set
+    if cache_old is not None:
+        # Diff the changed events
+        changed_events = sylvia.diff.get_updates(cache_old, cache_new)
+
+        # Join the changed events output with the RSS output
+        rss = sylvia.diff.join(rss, changed_events)
+
+    calendar_html = sylvia.render.as_html(rss)
+
+    return calendar_html
+
+def as_html(rss):
     return template.render(rss=rss,
                            print_date=print_date,
                            print_time=print_time,
