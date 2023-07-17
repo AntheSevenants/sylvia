4008838fdd1157d60e5872d75d002475704e7ad4
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-17 10:24:04 +0200

    Update date format

--- sylvia/helpers.py
+++ sylvia/helpers.py
@@ -5,6 +5,6 @@ brussels = pytz.timezone("Europe/Brussels")
 
 def get_current_date_time():
     now = datetime.now(brussels)
-    time_string = now.strftime("%-d %B %Y %H:%M")
+    time_string = now.strftime(f"%Y-%m-%d %H:%M")
 
     return time_string
