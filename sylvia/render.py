e95c1d387b633b59e8e97da226f53fb173bbc40c
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-17 11:02:45 +0200

    Add helpers for render functions

--- sylvia/render.py
+++ sylvia/render.py
@@ -14,25 +14,72 @@ env = Environment(
 template = env.get_template("calendar.html")
 
 def date_from_string(input_datetime):
+    """Get a datetime object from an RSS date
+
+    Args:
+        input_datetime (str): the RSS date field
+
+    Returns:
+        datetime: datetime parsed from RSS date
+    """
+
     input_datetime = input_datetime.split("+")[0]
     return datetime.strptime(input_datetime, f"%Y-%m-%dT%H:%M:%S")
 
 def print_date(input_datetime):
+    """Print the current date from a datetime object as DD Month YYYY
+
+    Args:
+        input_datetime (datetime): input datetime object
+
+    Returns:
+        str: datetime object rendered as DD Month YYYY
+    """
+
     event_timestamp = date_from_string(input_datetime)
 
     return event_timestamp.strftime("%-d %B %Y")
 
 def print_time(input_datetime):
+    """Print the current time from a datetime object as HH:MM
+
+    Args:
+        input_datetime (datetime): input datetime object
+
+    Returns:
+        str: datetime object rendered as HH:MM
+    """
+
     event_timestamp = date_from_string(input_datetime)
 
     return event_timestamp.strftime("%H:%M")
 
 def print_date_time(input_datetime):
+    """Print the current date and time from a datetime object as DD Month YYYY HH:MM
+
+    Args:
+        input_datetime (datetime): input datetime object
+
+    Returns:
+        str: datetime object rendered as DD Month YYYY HH:MM
+    """
+
     event_timestamp = date_from_string(input_datetime)
 
     return event_timestamp.strftime("%-d %B %Y %H:%M")
 
 def calendar(rss, cache_new, cache_old=None):
+    """Render the calendar as HTML
+
+    Args:
+        rss (dict): the full RSS feed representation
+        cache_new (dict): the cache representation of the current RSS state
+        cache_old (dict, optional): the cache representation of a previous RSS state. Defaults to None.
+
+    Returns:
+        str: the calendar as HTML
+    """
+
     # Only diff and enrich if cache is set
     if cache_old is not None:
         # Diff the changed events
@@ -46,6 +93,15 @@ def calendar(rss, cache_new, cache_old=None):
     return calendar_html
 
 def as_html(rss):
+    """Renders a calendar based on a single RSS representation of the current state of the RSS feed *and* possible changes
+
+    Args:
+        rss (dict): an enriched RSS representation of the current state of the RSS feed and possible changes
+
+    Returns:
+        str: the calendar as HTML
+    """
+
     return template.render(rss=rss,
                            print_date=print_date,
                            print_time=print_time,
