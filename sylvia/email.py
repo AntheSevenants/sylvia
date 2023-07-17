aa66baa8289edf2216a382493a68ca3157dca3e3
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-17 09:38:16 +0200

    Fix email encoding

--- sylvia/email.py
+++ sylvia/email.py
@@ -2,11 +2,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 def create(to_address, subject, html_body):
-    message = MIMEMultipart()
+    message = MIMEText(html_body, "html", _charset='UTF-8')
     message["To"] = to_address
     # Needed to make the email open in the editor
     message["X-Unsent"] = "1"
     message["Subject"] = subject
-    message.attach(MIMEText(html_body, "html"))
 
-    return message
+    return message.as_string()
