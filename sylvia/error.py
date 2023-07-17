2350f38ead7d59809699e6e022bc74d5273931d6
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-17 10:58:06 +0200

    Add error docstring

--- sylvia/error.py
+++ sylvia/error.py
@@ -1,2 +1,11 @@
 def generate(error_message):
+    """Generate an error message to be shown when a request is malformed
+
+    Args:
+        error_message (str): the specific error message
+
+    Returns:
+        str: the full error message including link to landing page
+    """
+
     return f"{error_message} If you came here by accident, click <a href=\"/\">here</a> to generate a new calendar."
