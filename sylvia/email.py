3520b31e08ad2a48a661e30158ac563f86636f81
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-17 10:57:20 +0200

    Add email docstring

--- sylvia/email.py
+++ sylvia/email.py
@@ -2,6 +2,17 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 def create(to_address, subject, html_body):
+    """Generate an EML containing the chosen calendar
+
+    Args:
+        to_address (str): To email address to be pre-filled
+        subject (str): subject to be pre-filled
+        html_body (str): HTML content of the email
+
+    Returns:
+        str: EML email (could be base64 encoded!)
+    """
+
     message = MIMEText(html_body, "html", _charset='UTF-8')
     message["To"] = to_address
     # Needed to make the email open in the editor
