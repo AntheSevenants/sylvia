c8d55374f9740f09d27fa82e8e5dda647e2d4c97
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-07-17 10:59:10 +0200

    Add helpers docstrings

--- sylvia/helpers.py
+++ sylvia/helpers.py
@@ -7,18 +7,33 @@ import sylvia.diff
 brussels = pytz.timezone("Europe/Brussels")
 
 def get_current_date_time():
+    """Get the current date and time as YYYY-MM-DD HH:MM
+
+    Returns:
+        str: the current date and time as YYYY-MM-DD HH:MM
+    """
+
     now = datetime.now(brussels)
     time_string = now.strftime(f"%Y-%m-%d %H:%M")
 
     return time_string
 
 def get_current_date():
+    """Get the current date as YYYY-MM-DD
+
+    Returns:
+        str: the current date and time as YYYY-MM-DD
+    """
+
     now = datetime.now(brussels)
     time_string = now.strftime(f"%Y-%m-%d")
 
     return time_string
 
 def clean_cache_files():
+    """Clean cache files in the set cache dir using the set max cache files
+    """
+
     # Remove old checkpoints
     cache_files = sylvia.diff.get_cache_files(os.environ["CACHE_DIR"])
     max_cache_files = int(os.environ["MAX_CACHE_FILES"])
