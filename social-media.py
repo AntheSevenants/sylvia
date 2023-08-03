546681689d835ffd20e2fd5062fa1caf49fbae86
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-08-03 09:15:03 +0200

    Update social media parent script

--- social-media.py
+++ social-media.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import sylvia.render
 import sylvia.diff
+import sylvia.socials
 
 parser = argparse.ArgumentParser(
     description="social media for sylvia - automatic posts on social media"
@@ -22,36 +23,6 @@ changed_events = sylvia.diff.get_updates(cache_old, cache_new)
 # Enrich RSS entries with changes
 rss_rich = sylvia.diff.join(rss, changed_events)
 
-# Time for character management :-) You're probably not paying for Twitter (X?) Blue
-medium = "twitter"
-TWEET_LENGTH = 280
-LINK_LENGTH = 23 + 1 # as per https://developer.twitter.com/en/docs/counting-characters
+posts = sylvia.socials.compose(rss_rich)
 
-for event in rss_rich:
-    if not "change" in event:
-        continue
-
-    body_text = ""
-    if event["change"] == "added":
-        body_text = f"🆕 New event! "
-    elif event["change"] == "changed":
-        if "date" in event["changes"]:
-            body_text = f"📅⚠️ Date change! "
-        elif "time" in event["changes"]:
-            body_text = f"⏰⚠️ Time change! "
-        else:
-            # Other changes do not matter
-            continue
-
-    chars_left = TWEET_LENGTH - len(body_text)
-    event_title = event["title"]
-
-    # We only truncate if the social medium is Twitter
-    if chars_left < len(event["title"]) and medium == "twitter":
-        print("Big problems!", event["title"])
-        # We truncate the title to the number of chars left, minus one for the …
-        event_title = event["title"][:chars_left - 1] + "…"
-
-    body_text = f"{body_text}{event_title} {event['link']}"
-
-    print(body_text)
+print(posts)
